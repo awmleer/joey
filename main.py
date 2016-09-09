81411e0a3d86ac4a8c61e5bf011a77ef6c54a0c0
haoguangbo <haoguangbo@yeah.net>
2016-09-09 16:24:05 +0800

    改用非阻塞式的Timer触发串口通信

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import serial,time,psutil
+from threading import Timer
 import mail
+
 ser = serial.Serial("/dev/cu.usbmodem1411")
 print(ser.name)
-while True:
-    # line = ser.readline()
-    #todo 为了监听串口回传的数据,不能使用sleep这种阻断的方式
-    time.sleep(0.5)
-    # 每5秒向窗口写一个hello
+
+# 使用count来记录process被调用的次数
+count=0
+
+def process():
+    global count
+    print(count)
+    if count==0:
+        mail_unseen()
+        print('mail')
+    else:
+        if count%2==0:
+            cpu()
+            print('cpu')
+        else:
+            memory()
+            print('memory')
+
+    count=(count+1)%120
+    Timer(1, process).start()
+
+Timer(0.5,process).start()
+
+def cpu():
     val = psutil.cpu_percent()
     ser.write(b"%dC"%int(val*180/100))  # 把CPU使用比换算成180°,传给Arduino
 
-    time.sleep(0.5)
+def memory():
     val = psutil.virtual_memory().percent
     ser.write(b"%dM" % int(val * 180 / 100))  # 把内存使用比换算成180°,传给Arduino
 
-    time.sleep(0.5)
+def mail_unseen():
     val = mail.count()
-    ser.write(b"%dA" % int(val * 180 / 100))  # 把未读邮件总数传给Arduino
+    ser.write(b"%dA" % val)  # 把未读邮件总数传给Arduino
+    print('mail'+str(val))
 
-ser.close()
+
+
+# ser.close()
